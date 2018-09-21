34442e3848c54bf443dc2f44547b8f9506779803
Yuwenbiao <2383299053@qq.com>
2018-09-21 13:42:37 +0800

    幽灵引用对象的使用示例

---
+++ src/Java虚拟机/引用类型/幽灵引用/UseReferenceQueue.java
@@ -0,0 +1,11 @@
+package Java虚拟机.引用类型.幽灵引用;
+
+/**
+ * 幽灵引用对象的使用示例
+ *
+ * @author yuwb
+ * @date 2018/9/21 13:41
+ */
+public class UseReferenceQueue {
+
+}
--- src/Java虚拟机/引用类型/弱引用/BookKeeper.java
+++ src/Java虚拟机/引用类型/弱引用/BookKeeper.java
@@ -1,5 +1,10 @@
 package Java虚拟机.引用类型.弱引用;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * HashMap类造成对象存活时间过长的示例
  *
@@ -7,4 +12,28 @@ package Java虚拟机.引用类型.弱引用;
  * @date 2018/9/18 10:29
  */
 public class BookKeeper {
+    private Map<Book, Set<User>> books = new HashMap<>();
+//    private WeakHashMap<Book, Set<User>> books = new WeakHashMap<>();
+
+    public void borrowBook(Book book, User user) {
+        Set<User> users = null;
+        if (books.containsKey(book)) {
+            users = books.get(book);
+        } else {
+            users = new HashSet<>();
+            books.put(book, users);
+        }
+        users.add(user);
+    }
+
+    public void returnBook(Book book, User user) {
+        if (books.containsKey(book)) {
+            Set<User> users = books.get(book);
+            users.remove(user);
+        }
+    }
 }
+
+class Book{}
+
+class User{}
